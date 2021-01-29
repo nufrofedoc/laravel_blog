795dd8b2a466380b450df3758dee9b2c56581c62
Nebado <ruslan.nbd@gmail.com>
2021-01-29 21:24:26 +0200

    Add CategoryController for controll categories, and add route for CategoryController

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', 'PostController')->names('blog.posts');
 });
 
+// Admin panel of blog
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix'    => 'admin/blog',
+];
+
+Route::group($groupData, function () {
+    // BlogCategory
+    $methods = ['index', 'edit', 'update', 'create', 'store'];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
+
 //Route::resource('rest', 'RestTestController'::class)->names('restTest');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
