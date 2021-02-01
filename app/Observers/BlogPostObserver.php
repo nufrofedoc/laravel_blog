93e9b776a5bacb7ab510eb1ceec8e5372aec3248
Nebado <ruslan.nbd@gmail.com>
2021-02-01 22:18:42 +0200

    Added an observers for posts and category.

---
+++ app/Observers/BlogPostObserver.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\BlogPost;
+use Carbon\Carbon;
+
+class BlogPostObserver
+{
+    /**
+     * Handle the BlogPost "created" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function created(BlogPost $blogPost)
+    {
+
+    }
+
+    /**
+     * Handle the BlogPost "updated" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function updated(BlogPost $blogPost)
+    {
+        $this->setPublishedAt($blogPost);
+
+        $this->setSlug($blogPost);
+    }
+
+    /**
+     * If date of publication isn't set, and setup a flag - Published,
+     * than setup date of publication on current date.
+     *
+     * @param BlogPost $blogPost
+     */
+    protected function setPublishedAt(BlogPost $blogPost)
+    {
+        if (empty($blogPost->published_at) && $blogPost->is_published) {
+            $blogPost->published_at = Carbon::now();
+        }
+    }
+
+    /**
+     * If the field is empty, than fill him by convertation heading.
+     *
+     * @param BlogPost $blogPost
+     */
+    protected function setSlug(BlogPost $blogPost)
+    {
+        if (empty($blogPost->slug)) {
+            $blogPost->slug = \Str::slug($blogPost->title);
+        }
+    }
+
+    /**
+     * Handle the BlogPost "deleted" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function deleted(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the BlogPost "restored" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function restored(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the BlogPost "force deleted" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function forceDeleted(BlogPost $blogPost)
+    {
+        //
+    }
+}
