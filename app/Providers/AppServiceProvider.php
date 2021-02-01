5d346e8c66bcbd03d9facccfeace2107ed9df796
Nebado <ruslan.nbd@gmail.com>
2021-02-01 22:21:23 +0200

    Added an observers in AppServiceProvider for observing models BlogPost and BlogCategory.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        BlogPost::observe(BlogPostObserver::class);
+        BlogCategory::observe(BlogCategoryObserver::class);
     }
 }
